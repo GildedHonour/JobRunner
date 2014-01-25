90715c3f0c43ecc260915c8e07999198e664a48f
Akshay Rawat <projects@akshay.cc>
2014-01-26 01:02:23 +0530

    minor bug fixes enhancements

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,15 +1,15 @@
 class Company < ActiveRecord::Base
 
-  has_many :addresses, -> { order "created_at ASC" }, dependent: :destroy, as: :addressable
+  has_many :addresses, -> { order "created_at" }, dependent: :destroy, as: :addressable
 
   # All affiliations
   has_many :affiliate_affiliations, foreign_key: 'principal_id', dependent: :destroy, class_name: 'Affiliation'
-  has_many :affiliates, through: :affiliate_affiliations, source: :affiliate, class_name: 'Company'
+  has_many :affiliates, -> { order "name" }, through: :affiliate_affiliations, source: :affiliate, class_name: 'Company'
 
   has_many :principal_affiliations, foreign_key: 'affiliate_id', dependent: :destroy, class_name: 'Affiliation'
   has_many :principals, through: :principal_affiliations, source: :principal, class_name: 'Company'
 
-  has_many :contacts, dependent: :destroy
+  has_many :contacts, -> { order "created_at" },  dependent: :destroy
   has_many :phone_numbers, -> { order "created_at ASC" }, as: :phonable, dependent: :destroy
 
   accepts_nested_attributes_for :addresses, reject_if: lambda { |address| address[:address_line_1].blank? }, allow_destroy: true
@@ -35,5 +35,9 @@ class Company < ActiveRecord::Base
     def all_principals
       Company.order("name").find(*Affiliation.pluck(:principal_id).uniq)
     end
+  end
+
+  def affiliate_affiliations_of_affiliate(company)
+    self.affiliate_affiliations.where(affiliate_id: company).order("created_at")
   end
 end
