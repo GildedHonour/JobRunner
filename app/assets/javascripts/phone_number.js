37d0f78e874052d7c36195793516a2e605db3867
Akshay Rawat <projects@akshay.cc>
2014-03-16 23:44:56 +0530

    phone number format bug

--- app/assets/javascripts/phone_number.js
+++ app/assets/javascripts/phone_number.js
@@ -1,4 +1,4 @@
-$(document).on('page:change', function() {
+$(document).on('page:change, nested:fieldAdded', function() {
     $('input[data=phone-number-input]').mask('(999) 999-9999');
     $('input[data=phone-number-extension-input]').mask('x 9?9999');
 });
