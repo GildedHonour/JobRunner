cbf6562dea123478e8491af04abc03d62883db63
Akshay Rawat <projects@akshay.cc>
2014-04-13 01:55:50 +0530

    add more recipients to audit trail report

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -1,9 +1,11 @@
 class SystemMailer < ActionMailer::Base
+  SYSTEM_RECIPIENTS = %w(projects@akshay.cc sean@engageyourcause.com lori@mmidirect.com)
+
   def daily_audit_report(date, report_csv_data)
     mail(subject: "Audit report for #{l(date, format: :only_date)}",
          reply_to: Jobrunner::Application.config.system_email,
          from: Jobrunner::Application.config.system_email,
-         to: "projects@akshay.cc"
+         to: SYSTEM_RECIPIENTS
     ) do |format|
         format.text do
           attachments["Audit report - #{l(date, format: :only_date)}.csv"] = { content: report_csv_data, mime_type: "text/csv" }
