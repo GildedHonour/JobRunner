5e14fc03c341d421bf21a0ea5cb0b933714cb2df
Akshay Rawat <projects@akshay.cc>
2014-05-22 00:16:11 +0530

    add admin to users

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,12 @@
 class ActivitiesController < ApplicationController
+  before_filter :authenticate_admin!
+
   def index
     @activities = Version.order("created_at DESC")
+  end
+
+  private
+  def authenticate_admin!
+    redirect_to root_url and return unless current_user.try(&:admin?)
   end
 end
