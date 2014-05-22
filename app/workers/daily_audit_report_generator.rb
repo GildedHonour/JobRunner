c289af83985505a61721348cbd3f81d9d7b95421
Akshay Rawat <projects@akshay.cc>
2014-05-22 11:42:33 +0530

    do not send audit email if no changes for the day

--- app/workers/daily_audit_report_generator.rb
+++ app/workers/daily_audit_report_generator.rb
@@ -1,7 +1,7 @@
 class DailyAuditReportGenerator
-  def perform params
+  def perform
     report_start_time = 1.day.ago.beginning_of_day
     no_of_changes = Version.where("created_at > ? AND created_at <= ?", report_start_time, report_start_time.end_of_day).size
-    SystemMailer.daily_audit_report(report_start_time, no_of_changes).deliver
+    SystemMailer.daily_audit_report(report_start_time, no_of_changes).deliver unless no_of_changes.zero?
   end
 end
