b7433091b269f871077370f8ac8ba542a73d9141
Akshay Rawat <projects@akshay.cc>
2014-05-22 12:26:40 +0530

    exclude a few more columns from audit

--- app/models/concerns/audited.rb
+++ app/models/concerns/audited.rb
@@ -2,7 +2,8 @@ module Audited
   extend ActiveSupport::Concern
   included do
     has_paper_trail ignore: [
-      :created_at, :updated_at, :last_sign_in_at, :current_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :sign_in_count
+      :created_at, :updated_at, :last_sign_in_at, :current_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :sign_in_count,
+      :encrypted_password, :invitation_token, :reset_password_token
     ], meta: {
         item_descriptor:      Proc.new { |object| object.audit_meta[:item_descriptor] },
         item_root_class:      Proc.new { |object| object.audit_meta[:item_root_class].to_s },
