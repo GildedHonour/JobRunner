50a021deae2a884c2fd5cdd9157db9464e960500
Akshay Rawat <projects@akshay.cc>
2014-05-22 12:07:26 +0530

    manage bugsnag through app, not heroku addons

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,4 +1,6 @@
-Bugsnag.configure do |config|
-  config.notify_release_stages = [ 'production' ]
-  config.use_ssl = true
+if  Rails.application.secrets.bugsnag_api_key.present?
+  Bugsnag.configure do |config|
+    config.notify_release_stages = [ 'production' ]
+    config.use_ssl = true
+  end
 end
