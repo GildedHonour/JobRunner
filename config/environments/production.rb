fa94563684823073899e916b485af43b7fe4ef96
Gilded Honour <me@gildedhonour.com>
2014-06-17 12:14:26 +0800

    heroku: reverted;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Jobrunner::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "//#{Rails.application.secrets.cloud_front_url}"
-  config.action_controller.asset_host = "//jobrunner-alex-test.herokuapp.com"
+  config.action_controller.asset_host = "//#{Rails.application.secrets.cloud_front_url}"
+  # config.action_controller.asset_host = "//jobrunner-alex-test.herokuapp.com"
   
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
