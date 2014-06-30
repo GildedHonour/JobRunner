37887907e96aac306c2242f9c8b0d15166dee8f1
Gilded Honour <me@gildedhonour.com>
2014-06-30 20:24:20 +0800

    errors: fixed;

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -4,6 +4,7 @@ function ready() {
   var modal = $("#myModal");
   var addresses = [];
 
+  //modal - use an address
   $(document).on("click", "#btn_choose_address", function() {
     var id = parseInt(modal.find(".modal-body input[type=radio]:checked").val(), 10); 
     $("#contact_info_container").html("This company also has <a href='#'>other addresses</a>.");
@@ -22,6 +23,7 @@ function ready() {
     }
   });
 
+  //modal - close
   $(document).on("click", "#close_modal", function() {
     if (
       $("#contact_addresses_attributes_0_address_line_1").val() === "" &&
@@ -52,7 +54,9 @@ function ready() {
     $("#contact_info_container").html("");
   };
 
-  setChkState();
+  function doesAddressFormExist() {
+    return $("div.col-md-4.fields:visible").length > 1;
+  };
 
   function resetAddressForm() {
     //todo - refactor
@@ -75,23 +79,46 @@ function ready() {
     $("#addresses :input").prop("disabled", true);
   };
 
-  //remove
-  $(document).on("click", "#addresses .btn.btn-danger.remove_nested_fields:visible:eq(0)", function() {
-    chkUseCompContInfo.prop("disabled", true);
-    chkUseCompContInfo.prop("checked", false);
-  });
+  setChkState();
 
   //add
   $(document).on("click", "div.row div.col-md-4 .btn.btn-default.add_nested_fields", function() {
     if ($("#contact_company_id").val() !== "") {
-      debugger;
       chkUseCompContInfo.prop("disabled", false);
     }
   });
 
+  //remove
+  $(document).on("click", "#addresses .btn.btn-danger.remove_nested_fields:visible:eq(0)", function() {
+    if (!doesAddressFormExist()) {
+      chkUseCompContInfo.prop("disabled", true);
+      chkUseCompContInfo.prop("checked", false);
+    }
+  });
+
+  //drop down
+  $(document).on("change", "#contact_company_id", function() {
+    if ((chkUseCompContInfo).is(":checked")) {
+      chkUseCompContInfo.prop("checked", false);
+    }
+
+    //company is selected
+    if ($("#contact_company_id").val() !== "") {
+      if (doesAddressFormExist()) {
+        chkUseCompContInfo.prop("disabled", false);
+      }
+    } else {
+      chkUseCompContInfo.prop("disabled", true);
+      chkUseCompContInfo.prop("checked", false);
+    }
+
+    //resetAddressForm();
+    enableAddressForm();
+    clearErrors();
+  });
+
   function enableAddressForm() {
     $("#addresses :input").prop("disabled", false);
-    $("#addresses .btn.btn-danger.remove_nested_fields").attr("disabled", false);
   };
 
   $(document).on("page:change", function() {
@@ -120,7 +147,6 @@ function ready() {
   });
 
   $(document).on("click", "#use_comp_cont_info", function() {
-    
     if (this.checked) {
       $.ajax({
         url: getAddressUrl($("#contact_company_id").val())
@@ -192,21 +218,6 @@ function ready() {
   $("#contact_info_container").on("click", "a", function(e) {
     e.preventDefault();
     $("#myModal").modal("show");
-  });
-
-  $(document).on("change", "#contact_company_id", function() {
-
-    //todo
-    // if there is no AddressForm then make it disabled
-
-    if ((chkUseCompContInfo).is(":checked")) {
-      chkUseCompContInfo.prop("checked", false);
-    };
-
-    clearErrors();
-    setChkState();
-    resetAddressForm();
-    enableAddressForm();
   });
 };
 
